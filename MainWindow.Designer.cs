261a580f3d346b5ca012772d840a44791dc6e109
Sophie Marie A <sylveon64@hotmail.com>
2023-02-11 19:06:08 -0600

    Initial commit

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.label1 = new System.Windows.Forms.Label();
+            this.currentFile = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.processStatus = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(12, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Current File:";
+            // 
+            // currentFile
+            // 
+            this.currentFile.AutoSize = true;
+            this.currentFile.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.currentFile.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.currentFile.Location = new System.Drawing.Point(81, 56);
+            this.currentFile.Name = "currentFile";
+            this.currentFile.Size = new System.Drawing.Size(43, 13);
+            this.currentFile.TabIndex = 1;
+            this.currentFile.Text = "currfile";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.button1.Location = new System.Drawing.Point(12, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(336, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Select File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.label3.Location = new System.Drawing.Point(12, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Status:";
+            // 
+            // processStatus
+            // 
+            this.processStatus.AutoSize = true;
+            this.processStatus.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.processStatus.Location = new System.Drawing.Point(60, 14);
+            this.processStatus.Name = "processStatus";
+            this.processStatus.Size = new System.Drawing.Size(78, 13);
+            this.processStatus.TabIndex = 5;
+            this.processStatus.Text = "processStatus";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(9, 80);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(259, 13);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Also check out Novetus! an old ROBLOX launcher";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.linkLabel2.Location = new System.Drawing.Point(290, 67);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(44, 13);
+            this.linkLabel2.TabIndex = 7;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "GitHub";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.linkLabel3.Location = new System.Drawing.Point(276, 80);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(74, 13);
+            this.linkLabel3.TabIndex = 8;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "system64.dev";
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(362, 109);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.processStatus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.currentFile);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.Text = "Place Compressor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label currentFile;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label processStatus;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+    }
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,100 @@
+﻿#region Using's
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+#endregion
+
+namespace WindowsFormsApp3
+{
+    public partial class MainWindow : Form
+    {
+        private string currFile;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            currentFile.Text = "No file selected!";
+            processStatus.Text = "Idle";
+        }
+
+        public void Compress(string currFile) 
+        {
+            var newFile = currFile + ".bz2";
+            var input = File.OpenRead(currFile);
+            var output = File.Create(newFile);
+            /*
+            if (File.Exists(newFile))
+            {
+                MessageBox.Show("The file " + newFile + " already exists, are you sure you want to continue? This will overwrite the existing file.","Map Compressor - Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            } // If the output file already exists, Prompt the user weather or not they want to continue. 
+            else
+            */
+            {
+                try
+                {
+                    {
+                        processStatus.Text = "Compressing...";
+
+                        using (var compressor = new Ionic.BZip2.ParallelBZip2OutputStream(output))
+                        {
+                            byte[] buffer = new byte[2048];
+                            int n;
+                            while ((n = input.Read(buffer, 0, buffer.Length)) > 0)
+                            {
+                                compressor.Write(buffer, 0, n);
+                            }
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    // MessageBox.Show(Exception.Message, "Place Compressor - Oh Noes!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    throw;
+                }
+                
+                finally
+                {
+                processStatus.Text = "Compression finished!";
+                }
+                
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            {
+                ofd.Filter = "Roblox Level (*.rbxl)|*.rbxl|Roblox Level (*.rbxlx)|*.rbxlx";
+                ofd.FilterIndex = 1;
+                ofd.Title = "Load Roblox Level...";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    currentFile.Text = ofd.SafeFileName;
+                    currFile = Path.GetFullPath(ofd.FileName);
+                    Compress(currFile);
+                }
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://bitl.itch.io/novetus");
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/IDeletedSystem64/rblx-compressor");
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://system64.dev");
+        }
+    }
+}
